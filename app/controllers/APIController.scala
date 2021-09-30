74f2d60b262398bdb79e37a31a6a5e1e7b523352
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2021-09-30 09:07:37 +0100

    [PRM-690] - Endpoint to expose API

---
+++ app/controllers/APIController.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import services.ETMPService
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import utils.RegimeHelper
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class APIController @Inject()(etmpService: ETMPService,
+                              cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends BackendController(cc) {
+
+  def getSummaryDataForVRN(vrn: String): Action[AnyContent] = Action.async {
+    implicit request => {
+      val enrolmentKey = RegimeHelper.constructMTDVATEnrolmentKey(vrn)
+      etmpService.getPenaltyDataFromETMPForEnrolment(enrolmentKey).map {
+        _._1.fold(
+          NotFound(s"Unable to find data for VRN: $vrn")
+        )(
+          etmpPayload => Ok("")
+        )
+      }
+    }
+  }
+}
---
+++ it/controllers/APIControllerISpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.http.Status
+import play.api.test.Helpers._
+import utils.{ETMPWiremock, IntegrationSpecCommonBase}
+
+class APIControllerISpec extends IntegrationSpecCommonBase with ETMPWiremock {
+  val controller: APIController = injector.instanceOf[APIController]
+
+  "getSummaryDataForVRN" should {
+    s"return OK (${Status.OK})" when {
+      "the ETMP call succeeds" in {
+        mockResponseForStubETMPPayload(Status.OK, "HMRC-MTD-VAT~VRN~123456789")
+        val result = await(buildClientForRequestToApp(uri = "/vat/penalties/summary/123456789").get)
+        result.status shouldBe OK
+        //TODO: change data based on implementation
+        result.body shouldBe ""
+      }
+    }
+
+    s"return OK (${Status.NOT_FOUND})" when {
+      "the ETMP call fails" in {
+        mockResponseForStubETMPPayload(Status.INTERNAL_SERVER_ERROR, "HMRC-MTD-VAT~VRN~123456789", body = Some(""))
+        val result = await(buildClientForRequestToApp(uri = "/vat/penalties/summary/123456789").get)
+        result.status shouldBe NOT_FOUND
+      }
+
+      "the ETMP call returns nothing" in {
+        mockResponseForStubETMPPayload(Status.OK, "HMRC-MTD-VAT~VRN~123456789", body = Some("{}"))
+        val result = await(buildClientForRequestToApp(uri = "/vat/penalties/summary/123456789").get)
+        result.status shouldBe NOT_FOUND
+      }
+    }
+  }
+}
