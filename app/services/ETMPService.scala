bc336cf30d86c82882e209d8432c4309eef10df9
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2021-10-01 15:29:31 +0100

    [PRM-525] - Hotfix for testing PEGA spec changes

--- app/services/ETMPService.scala
+++ app/services/ETMPService.scala
@@ -21,6 +21,7 @@ import connectors.{AppealsConnector, ETMPConnector}
 import models.ETMPPayload
 import models.appeals.AppealSubmission
 import models.point.PointStatusEnum
+import play.api.libs.json.Json
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import utils.Logger.logger
 
@@ -99,7 +100,9 @@ class ETMPService @Inject()(etmpConnector: ETMPConnector,
 
   def submitAppeal(appealSubmission: AppealSubmission,
                    enrolmentKey: String, isLPP: Boolean, penaltyId: String)(
-    implicit ec: ExecutionContext, hc: HeaderCarrier): Future[HttpResponse] = {appealsConnector.submitAppeal(appealSubmission, enrolmentKey, isLPP, penaltyId)
+    implicit ec: ExecutionContext, hc: HeaderCarrier): Future[HttpResponse] = {
+    logger.debug(s"[ETMPService][submitAppeal] - Sending appeal with payload: ${Json.toJson(appealSubmission)(AppealSubmission.apiWrites)}")
+    appealsConnector.submitAppeal(appealSubmission, enrolmentKey, isLPP, penaltyId)
   }
 
   def getNumberOfEstimatedPenalties(etmpPayload: ETMPPayload): Int = {
