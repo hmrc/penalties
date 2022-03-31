0e0fe445394aec141b1c38737aba9daca529d55a
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-03-31 08:28:59 +0100

    [PRM-1211] - Upgrade to latest dependencies and Scala/SBT

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,20 +8,20 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.17.0",
-    "uk.gov.hmrc"             %% "emailaddress"               % "3.5.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.21.0",
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"   % "5.17.0"  % "test",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"     % "0.58.0" % "test, it",
-    "org.scalatest"           %% "scalatest"                   % "3.0.8"   % "test",
-    "com.typesafe.play"       %% "play-test"                   % current   % "test",
-    "org.pegdown"             %  "pegdown"                     % "1.6.0"   % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"   % "5.21.0"  % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"     % "0.62.0"  % "test, it",
+    "org.scalatest"           %% "scalatest"                   % "3.2.11"  % "test, it",
+    "com.typesafe.play"       %% "play-test"                   % current   % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"          % "5.1.0"   % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-jre8"               % "2.26.3"  % "it",
-    "org.scalamock"           %% "scalamock-scalatest-support" % "3.6.0"   % "test",
-    "com.github.tomakehurst"  %  "wiremock-standalone"         % "2.22.0"  % "it",
-    "org.mockito"             % "mockito-core"                 % "3.1.0"   % "test, it"
+    "com.github.tomakehurst"  %  "wiremock-jre8"               % "2.32.0"  % "it",
+    "org.scalamock"           %% "scalamock-scalatest-support" % "3.6.0"   % "test, it",
+    "com.vladsch.flexmark"     % "flexmark-all"                  % "0.62.2"  % "test, it",
+    "org.mockito"             % "mockito-core"                 % "4.4.0"   % "test, it",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"           % "2.13.2"             % "test, it",
+    "com.fasterxml.jackson.core"   %  "jackson-databind"               % "2.13.2.1"           % "test, it"
   )
 }
