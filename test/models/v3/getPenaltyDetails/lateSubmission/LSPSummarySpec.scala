3e03571043a862a893f101478397c5dad1e9fa58
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-04-13 16:08:28 +0100

    [PRM-1259] - Added new API 1812 model

---
+++ test/models/v3/getPenaltyDetails/lateSubmission/LSPSummarySpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.v3.getPenaltyDetails.lateSubmission
+
+import base.SpecBase
+import play.api.libs.json.{JsObject, JsResult, JsValue, Json}
+
+class LSPSummarySpec extends SpecBase {
+
+  val jsonRepresentingModel: JsValue = Json.parse(
+    """
+      |{
+      |   "activePenaltyPoints": 10,
+      |   "inactivePenaltyPoints": 12,
+      |   "PoCAchievementDate": "2069-10-30",
+      |   "regimeThreshold": 10,
+      |   "penaltyChargeAmount": 684.25
+      |}
+      |""".stripMargin)
+
+  val model: LSPSummary = LSPSummary(
+    activePenaltyPoints = 10,
+    inactivePenaltyPoints = 12,
+    regimeThreshold = 10,
+    penaltyChargeAmount = 684.25
+  )
+
+  "be readable from JSON" in {
+    val result: JsResult[LSPSummary] = Json.fromJson(jsonRepresentingModel)(LSPSummary.format)
+    result.isSuccess shouldBe true
+    result.get shouldBe model
+  }
+
+  "be writable to JSON" in {
+    val result: JsValue = Json.toJson(model)(LSPSummary.format)
+    result shouldBe jsonRepresentingModel.as[JsObject] - "PoCAchievementDate"
+  }
+}
---
+++ test/models/v3/getPenaltyDetails/lateSubmission/LateSubmissionSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.v3.getPenaltyDetails.lateSubmission
+
+import base.SpecBase
+import play.api.libs.json.{JsResult, JsValue, Json}
+
+import java.time.LocalDate
+
+class LateSubmissionSpec extends SpecBase {
+  val jsonRepresentingModel: JsValue = Json.parse(
+    """
+      |{
+      | "taxPeriodStartDate": "2022-01-01",
+      | "taxPeriodEndDate": "2022-12-31",
+      | "taxPeriodDueDate": "2023-02-07",
+      | "returnReceiptDate": "2023-02-01"
+      |}
+      |""".stripMargin)
+
+  val model: LateSubmission = LateSubmission(
+    taxPeriodStartDate = Some(LocalDate.of(2022, 1, 1)),
+    taxPeriodEndDate = Some(LocalDate.of(2022, 12, 31)),
+    taxPeriodDueDate = Some(LocalDate.of(2023, 2, 7)),
+    returnReceiptDate = Some(LocalDate.of(2023, 2, 1))
+  )
+
+  "be readable from JSON" in {
+    val result: JsResult[LateSubmission] = Json.fromJson(jsonRepresentingModel)(LateSubmission.format)
+    result.isSuccess shouldBe true
+    result.get shouldBe model
+  }
+
+  "be writable to JSON" in {
+    val result: JsValue = Json.toJson(model)(LateSubmission.format)
+    result shouldBe jsonRepresentingModel
+  }
+}
