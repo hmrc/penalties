a0b97ea0aff769439ca99acbafc77c7cad303555
BackRowOla <56401572+BackRowOla@users.noreply.github.com>
2022-06-01 11:13:06 +0100

    [PRM-1385]: updated dependencies (#140)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.21.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.21.0"  % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.24.0"  % "test, it",
     "org.mockito"             % "mockito-all"               % "1.10.19" % "test, it"
   )
 }
