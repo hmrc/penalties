252ba9dbc6811186902fcc015b4fce3c58b9aa5d
Lewis Mitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-06-23 10:17:15 +0100

    [PRM-1464] - Added connector for 3rd party call to API 1811 (#150)

--- app/connectors/v3/getFinancialDetails/GetFinancialDetailsConnector.scala
+++ app/connectors/v3/getFinancialDetails/GetFinancialDetailsConnector.scala
@@ -18,23 +18,52 @@ package connectors.v3.getFinancialDetails
 
 import java.time.LocalDate
 import java.util.UUID.randomUUID
-
 import config.AppConfig
 import connectors.parsers.v3.getFinancialDetails.GetFinancialDetailsParser.GetFinancialDetailsResponse
+import play.api.http.Status.INTERNAL_SERVER_ERROR
 import javax.inject.Inject
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
-
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse, UpstreamErrorResponse}
+import utils.Logger.logger
+import uk.gov.hmrc.http.HttpReads.Implicits._
 import scala.concurrent.{ExecutionContext, Future}
 
 class GetFinancialDetailsConnector @Inject()(httpClient: HttpClient,
                                              appConfig: AppConfig)
                                             (implicit ec: ExecutionContext) {
 
-  def getFinancialDetails(vrn: String, dateFrom: LocalDate, dateTo: LocalDate)(implicit hc: HeaderCarrier): Future[GetFinancialDetailsResponse] = {
-    val headers = Seq("Authorization" -> s"Bearer ${appConfig.eiOutboundBearerToken}",
-      "CorrelationId" -> randomUUID().toString, "Environment" -> appConfig.eisEnvironment)
+  private val headers = Seq("Authorization" -> s"Bearer ${appConfig.eiOutboundBearerToken}",
+    "CorrelationId" -> randomUUID().toString, "Environment" -> appConfig.eisEnvironment)
 
+  def getFinancialDetails(vrn: String, dateFrom: LocalDate, dateTo: LocalDate)(implicit hc: HeaderCarrier): Future[GetFinancialDetailsResponse] = {
     httpClient.GET[GetFinancialDetailsResponse](url =
       appConfig.getFinancialDetailsUrlv3(vrn) + appConfig.queryParametersForGetFinancialDetail(dateFrom, dateTo), headers = headers)
+  }
+
+  def getFinancialDetailsForAPI(vrn: String,
+                                docNumber: Option[String],
+                                dateFrom: Option[String],
+                                dateTo: Option[String],
+                                onlyOpenItems: Boolean,
+                                includeStatistical: Boolean,
+                                includeLocks: Boolean,
+                                calculateAccruedInterest: Boolean,
+                                removePOA: Boolean,
+                                customerPaymentInformation: Boolean)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+    val queryParams: String = {
+      s"?${docNumber.fold("")(docNumber => s"docNumber=$docNumber&")}${dateFrom.fold("")(dateFrom => s"dateFrom=$dateFrom&")}" +
+        s"${dateTo.fold("")(dateTo => s"dateTo=$dateTo&")}onlyOpenItems=$onlyOpenItems&includeStatistical=$includeStatistical" +
+        s"&includeLocks=$includeLocks&calculateAccruedInterest=$calculateAccruedInterest&removePOA=$removePOA" +
+        s"&customerPaymentInformation=$customerPaymentInformation"
+    }
+    httpClient.GET[HttpResponse](appConfig.getFinancialDetailsUrlv3(vrn) + queryParams, headers = headers).recover {
+      case e: UpstreamErrorResponse => {
+        logger.error(s"[GetFinancialDetailsConnector][getFinancialDetailsForAPI] - Received ${e.statusCode} status from API 1811 call - returning status to caller")
+        HttpResponse(e.statusCode, e.message)
+      }
+      case e: Exception => {
+        logger.error(s"[GetFinancialDetailsConnector][getFinancialDetailsForAPI] - An unknown exception occurred - returning 500 back to caller - message: ${e.getMessage}")
+        HttpResponse(INTERNAL_SERVER_ERROR, "An unknown exception occurred. Contact the Penalties team for more information.")
+      }
+    }
   }
 }
