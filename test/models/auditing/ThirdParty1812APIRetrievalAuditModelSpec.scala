f9ff88988a10c15e36d1edc0d929c95c450cb013
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-08-18 12:04:56 +0100

    [PRM-1683] - Added 3rd party audit for API 1812

---
+++ test/models/auditing/ThirdParty1812APIRetrievalAuditModelSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.auditing
+
+import base.{LogCapturing, SpecBase}
+import play.api.http.Status
+import play.api.libs.json.{JsString, JsValue, Json}
+
+class ThirdParty1812APIRetrievalAuditModelSpec extends SpecBase with LogCapturing {
+  val sampleJsonResponse: JsValue = Json.toJson(
+    "foo" -> "bar"
+  )
+  val sampleJsonResponseAsString: String = Json.stringify(sampleJsonResponse)
+
+  val modelWithJsonifiedBody = ThirdParty1812APIRetrievalAuditModel("123456789", Status.OK, sampleJsonResponseAsString)
+  val modelWithStringBody = ThirdParty1812APIRetrievalAuditModel("123456789", Status.INTERNAL_SERVER_ERROR, "An error occurred")
+
+  "UserHasPenaltyAuditModel" should {
+    "have the correct audit type" in {
+      modelWithStringBody.auditType shouldBe "Penalties3rdPartyPenaltyDetailsDataRetrieval"
+    }
+
+    "have the correct transaction name" in {
+      modelWithStringBody.transactionName shouldBe "penalty-penalty-data-retrieval"
+    }
+
+    "show the correct audit details" when {
+      "the correct detail information is present (when the audit has a JSON body)" in {
+        (modelWithJsonifiedBody.detail \ "vrn").validate[String].get shouldBe "123456789"
+        (modelWithJsonifiedBody.detail \ "responseCodeSentAPIService").validate[Int].get shouldBe Status.OK
+        (modelWithJsonifiedBody.detail \ "etmp-response").validate[JsValue].get shouldBe sampleJsonResponse
+      }
+
+      "the correct detail information is present (when the audit has a non-JSON body)" in {
+        (modelWithStringBody.detail \ "vrn").validate[String].get shouldBe "123456789"
+        (modelWithStringBody.detail \ "responseCodeSentAPIService").validate[Int].get shouldBe Status.INTERNAL_SERVER_ERROR
+        (modelWithStringBody.detail \ "etmp-response").validate[JsValue].get shouldBe JsString("An error occurred")
+      }
+    }
+  }
+}
