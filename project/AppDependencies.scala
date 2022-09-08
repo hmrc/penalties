9a5b11bb9ed46cad27a6f7d51f61fc03f26796b4
Andrew Butcher <6122384+AndrewRButcher@users.noreply.github.com>
2022-09-08 15:11:32 +0100

    PRM-1749 Updated dependencies + Logging (#177)
    * PRM-1749 Updated dependencies + Logging
    
    * Revert Logging change
    
    * Updated Logging

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,14 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.2.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.24.0"  % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion  % "test, it",
     "org.mockito"             % "mockito-all"               % "1.10.19" % "test, it"
   )
 }
