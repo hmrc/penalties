59936e618055b98b1cd2b3e11ef9f100b840f03d
JBlacklaws <58850837+JBlacklaws@users.noreply.github.com>
2023-06-13 14:37:36 +0100

    [PRM-2625] Removed feature switch for filtering

--- app/config/featureSwitches/FeatureSwitch.scala
+++ app/config/featureSwitches/FeatureSwitch.scala
@@ -22,7 +22,7 @@ sealed trait FeatureSwitch {
 
 object FeatureSwitch {
   val prefix: String = "feature.switch"
-  val listOfAllFeatureSwitches: List[FeatureSwitch] = List(CallPEGA, CallDES, CallAPI1812ETMP, CallAPI1811ETMP, UseInternalAuth, SanitiseFileName, Filter9xAppealStatus)
+  val listOfAllFeatureSwitches: List[FeatureSwitch] = List(CallPEGA, CallDES, CallAPI1812ETMP, CallAPI1811ETMP, UseInternalAuth, SanitiseFileName)
 }
 
 case object CallPEGA extends FeatureSwitch {
@@ -47,8 +47,4 @@ case object UseInternalAuth extends FeatureSwitch {
 
 case object SanitiseFileName extends FeatureSwitch {
   override val name: String = s"${FeatureSwitch.prefix}.sanitise-file-name"
-}
-
-case object Filter9xAppealStatus extends FeatureSwitch {
-  override val name: String = s"${FeatureSwitch.prefix}.filter-9x-appeals"
 }
