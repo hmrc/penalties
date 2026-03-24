c9cf44ed22d43be4e79bb479e9725d726a30abea
Jacob Raffe <jakeraffe24@gmail.com>
2026-03-24 13:31:54 +0000

    Revert "[DL-18489] Improve null safety in FilterService (#339)" (#347)
    This reverts commit 79ba78d4dcc3dcd11150ba06b9ff83aa41e523e9.

--- app/utils/PenaltyDetailsConverter.scala
+++ app/utils/PenaltyDetailsConverter.scala
@@ -262,7 +262,4 @@ object PenaltyDetailsConverter {
       PoCAchievementDate = hipSummary.pocAchievementDate
     )
   }
-
-  def putSeqInsideOption[A](seq: Seq[A]): Option[Seq[A]] = if (seq.nonEmpty) Some(seq) else None
-
 }
