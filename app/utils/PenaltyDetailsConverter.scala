79ba78d4dcc3dcd11150ba06b9ff83aa41e523e9
Jacob Raffe <jakeraffe24@gmail.com>
2026-03-24 12:58:11 +0000

    [DL-18489] Improve null safety in FilterService (#339)

--- app/utils/PenaltyDetailsConverter.scala
+++ app/utils/PenaltyDetailsConverter.scala
@@ -262,4 +262,7 @@ object PenaltyDetailsConverter {
       PoCAchievementDate = hipSummary.pocAchievementDate
     )
   }
+
+  def putSeqInsideOption[A](seq: Seq[A]): Option[Seq[A]] = if (seq.nonEmpty) Some(seq) else None
+
 }
